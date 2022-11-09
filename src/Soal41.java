77d4a1983b0b3630165cc3e7f9b300d7070619a3
williamtanuwijaya <will.tw0807@gmail.com>
2022-11-09 22:30:56 +0700

    mengerjakan soal 41

---
+++ src/Soal41.java
@@ -0,0 +1,10 @@
+public class Soal41 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 4 ;i++) {
+            for(int j=1;j <=7 ; j++) {
+                System.out.print(i +" ");
+            }
+            System.out.println();
+        }
+    }
+}
