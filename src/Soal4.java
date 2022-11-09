67acefd7c88bbdceb5d0b6c67cc510471e3684b5
williamtanuwijaya <will.tw0807@gmail.com>
2022-11-09 20:59:40 +0700

    mengerjakan soal 3 sampai 5

---
+++ src/Soal4.java
@@ -0,0 +1,9 @@
+public class Soal4 {
+    public static void main(String[] args) {
+        for(int i=30; i<120;i++) {
+		if(i%2==1) {
+			System.out.println(i);
+		}
+	  }
+    }
+}
---
+++ src/Soal5.java
@@ -0,0 +1,9 @@
+public class Soal5 {
+    public static void main(String[] args) {
+        for(int i=31; i<120;i++) {
+		if(i%2==0) {
+			System.out.println(i);
+		}
+	  }
+    }
+}
