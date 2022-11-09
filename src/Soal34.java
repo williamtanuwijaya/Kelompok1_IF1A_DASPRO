1cdecf4ab3759f1f18f9d41ad6c10915b297540f
williamtanuwijaya <will.tw0807@gmail.com>
2022-11-09 22:24:02 +0700

    mengerjakan soal 34

---
+++ src/Soal34.java
@@ -0,0 +1,14 @@
+public class Soal34 {
+    public static void main(String[] args) {
+        for (int i = 1 ; i <=3; i++) {
+            for (int j = 1; j <= 10; j++) {
+                if( i == 1 || i ==2 || i == 3 ) {
+                    System.out.print("1 ");
+                }else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
