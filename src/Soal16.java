ce55414cb0412b43aadb5981362793b67098d62c
williamtanuwijaya <will.tw0807@gmail.com>
2022-11-09 21:51:00 +0700

    mengerjakan soal 16

---
+++ src/Soal16.java
@@ -0,0 +1,12 @@
+public class Soal16 {
+    public static void main(String[] args) {
+        for(int i=1;i<=6;i++) {
+		 for(int j=1;j<=6;j++) {
+			System.out.print(j+" ");
+			if(i==j) {
+				break;
+			}
+		}
+	  }
+    }
+}
