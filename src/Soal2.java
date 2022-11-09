d029d2a63a85b4244ccea055954029b1968188c6
williamtanuwijaya <will.tw0807@gmail.com>
2022-11-09 20:53:48 +0700

    mengerjakan soal 1 dan 2

--- src/Soal2.java
+++ src/Soal2.java
@@ -1,7 +1,7 @@
-public class Soal1 {
+public class Soal2 {
     public static void main(String[] args) {
         for (int i=0; i<=100; i++){
-            System.out.println("Hello World");
+            System.out.println(i);
         }
     }
 }
