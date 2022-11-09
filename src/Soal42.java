9ff66c4a5ce528ee7fbdba3f33e536cb7864a160
williamtanuwijaya <will.tw0807@gmail.com>
2022-11-09 22:32:11 +0700

    mengerjakan soal 42

---
+++ src/Soal42.java
@@ -0,0 +1,10 @@
+public class Soal42 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5 ;i++) {
+            for(int j=1;j <=7 ; j++) {
+                System.out.print(j +" ");
+            }
+            System.out.println();
+        }
+    }
+}
