dbe576bb18bbada1536434dff45c0b6e9c787f04
williamtanuwijaya <will.tw0807@gmail.com>
2022-11-09 22:29:32 +0700

    mengerjakan soal 40

---
+++ src/Soal40.java
@@ -0,0 +1,14 @@
+public class Soal40 {
+    public static void main(String[] args) {
+        for (int i = 1 ; i <=3; i++) {
+            for (int j = 1; j <= 5; j++) {
+                if( i == 1 || i ==2 || i == 3 ) {
+                    System.out.print("1 ");
+                }else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
