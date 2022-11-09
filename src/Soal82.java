c1410eb6c54ad55a6025ec2ecfd5e2452779cc4f
williamtanuwijaya <will.tw0807@gmail.com>
2022-11-09 22:15:31 +0700

    mengajarkan soal 82

---
+++ src/Soal82.java
@@ -0,0 +1,13 @@
+public class Soal82 {
+    public static void main(String[] args) {
+        for(int i=1;i<=6;i++) {
+            for(int j=1;j<=6;j++) {
+                if((i+j)==7) {
+                    System.out.print("* ");
+                }else {
+                    System.out.print("0 ");
+                }
+            }System.out.println();
+        }
+    }
+}
