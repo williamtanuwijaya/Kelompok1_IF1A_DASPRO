c8a0f9db8908c109017317926e27644a4dfb87da
williamtanuwijaya <will.tw0807@gmail.com>
2022-11-09 21:40:52 +0700

    mengerjakan soal 14

---
+++ src/Soal14.java
@@ -0,0 +1,12 @@
+public class Soal14 {
+    public static void main(String[] args) {
+        for(int i=1;i<=7;i++) {
+		  for(int j=1;j<=7;j++) {
+			 System.out.print(i+" ");
+			   if(i==j) {
+				break;
+			}
+		}
+	  }
+    }
+}
