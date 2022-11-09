c92dd179552a80facdaa3253a16bc7849bf267d0
williamtanuwijaya <will.tw0807@gmail.com>
2022-11-09 22:12:03 +0700

    mengerjakan soal 68

---
+++ src/Soal68.java
@@ -0,0 +1,15 @@
+public class Soal68 {
+    public static void main(String[] args) {
+        for(int i=1;i<=7;i++) {
+            for(int j=1;j<=7;j++) {
+                if((i+j)==8) {
+                    System.out.print("*");
+                }else if(i==j) {
+                    System.out.print("*");
+                }else {
+                    System.out.print(" ");
+                }
+            }System.out.println();
+        }
+    }
+}
