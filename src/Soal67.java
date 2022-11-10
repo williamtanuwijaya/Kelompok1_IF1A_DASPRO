a6c9880bd846d696da75501e0cfc271b2ccc8566
williamtanuwijaya <will.tw0807@gmail.com>
2022-11-10 21:51:13 +0700

    revisi soal 67

--- src/Soal67.java
+++ src/Soal67.java
@@ -1,7 +1,7 @@
 public class Soal67 {
     public static void main(String[] args) {
-        for (int i = 1; i <= 6 ;i++) {
-            for(int j=1;j <=6 ; j++) {
+        for (int i = 1; i <= 7 ;i++) {
+            for(int j=1;j <=7 ; j++) {
                 if( i == j ) {
                     System.out.print("* ");
                 }else {
