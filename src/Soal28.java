e2873f27dd14c1bed8bb8ef7b6fca5c087693172
williamtanuwijaya <will.tw0807@gmail.com>
2022-11-10 12:43:45 +0700

    mengerjakan soal 28

---
+++ src/Soal28.java
@@ -0,0 +1,9 @@
+public class Soal28 {
+    public static void main(String[] args) {
+        for(int i=5;i<=15;i+=5) {
+            System.out.print(i + " ");
+            i = i - 3;
+            System.out.print(i + " ");
+        }
+    }
+}
