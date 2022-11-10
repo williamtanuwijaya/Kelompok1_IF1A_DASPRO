a761336d2f423f5a30f93af6df1d2826015d0af8
williamtanuwijaya <will.tw0807@gmail.com>
2022-11-10 12:55:16 +0700

    mengerjakan soal 35

---
+++ src/Soal35.java
@@ -0,0 +1,10 @@
+public class Soal35 {
+    public static void main(String[] args) {
+        for(int i=4;i<=10;i+=3) {
+            for(int j=3;j<=i;j++) {
+                System.out.print("1 ");
+
+            }System.out.println("1");
+        }System.out.println();
+    }
+}
