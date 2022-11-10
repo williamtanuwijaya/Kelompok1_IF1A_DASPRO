93c2cc399ccb93be84c922dd5fc210d9730d6335
williamtanuwijaya <will.tw0807@gmail.com>
2022-11-10 13:03:09 +0700

    mengerjakan soal 29

---
+++ src/Soal29.java
@@ -0,0 +1,9 @@
+public class Soal29 {
+    public static void main(String[] args) {
+        for(int i=3;i<=120;i-=5) {
+            System.out.print(i + " ");
+            i = i * 3;
+            System.out.print(i + " ");
+        }
+    }
+}
