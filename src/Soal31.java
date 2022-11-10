3c7426486901b50098ca92747644e0bdd82b6ffd
williamtanuwijaya <will.tw0807@gmail.com>
2022-11-10 13:07:56 +0700

    mengerjakan soal 31

---
+++ src/Soal31.java
@@ -0,0 +1,9 @@
+public class Soal31 {
+    public static void main(String[] args) {
+        for(int i=1;i<=512;i=i*2) {
+            System.out.print(i + " ");
+            if (i == 512)break;
+
+        }
+    }
+}
