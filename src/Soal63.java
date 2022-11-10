8db4c4a6903e6533df6eb9069ed599cc2fbea25d
williamtanuwijaya <will.tw0807@gmail.com>
2022-11-10 13:27:46 +0700

    mengerjakan soal 63

---
+++ src/Soal63.java
@@ -0,0 +1,13 @@
+public class Soal63 {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++) {
+            for(int j=1;j<=3;j++) {
+                if(i==1||i==3||i==5||j==1||j==3) {
+                    System.out.print("* ");
+                }else {
+                    System.out.print("  ");
+                }
+            }System.out.println();
+        }
+    }
+}
