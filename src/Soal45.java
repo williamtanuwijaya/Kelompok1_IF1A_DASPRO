1b8a66f3984c3ebb80ad507655096e1529bb8e2d
williamtanuwijaya <will.tw0807@gmail.com>
2022-11-10 13:08:57 +0700

    mengerjakan soal 45

---
+++ src/Soal45.java
@@ -0,0 +1,11 @@
+public class Soal45 {
+    public static void main(String[] args) {
+        for(int i=1;i<=3;i++) {
+            for(int j=1;j<=7;j++) {
+
+                System.out.print(i+"."+j+" ");
+
+            }System.out.println();
+        }System.out.println();
+    }
+}
