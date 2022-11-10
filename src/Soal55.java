7a50f20962149c26d795ed7198d4fa3a21e278d1
williamtanuwijaya <will.tw0807@gmail.com>
2022-11-10 12:09:09 +0700

    mengerjakan soal 55

---
+++ src/Soal55.java
@@ -0,0 +1,15 @@
+public class Soal55 {
+    public static void main(String[] args) {
+        for (int i = 1 ; i <= 2 ; i++) {
+            for (int j = 1 ; j <=5 ; j++) {
+                if (i ==1) {
+                    System.out.print("+ ");
+                } else {
+                    System.out.print("0 ");
+                }
+
+            }
+            System.out.println();
+        }
+    }
+}
