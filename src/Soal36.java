27cbca3dbe697f7fe3409274d197ab80a445bc99
williamtanuwijaya <will.tw0807@gmail.com>
2022-11-10 19:46:37 +0700

    mengerjakan soal 36

--- src/Soal36.java
+++ src/Soal36.java
@@ -1,5 +1,9 @@
 public class Soal36 {
     public static void main(String[] args) {
-
+        for(int i=2;i<=16;i+=7) {
+            System.out.print(i+" ");
+            i=i-5;
+            System.out.print(i+" ");
+        }
     }
 }
