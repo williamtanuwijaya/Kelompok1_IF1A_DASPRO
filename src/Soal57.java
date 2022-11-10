a608b6975b7ca301c062126299363ac91e859095
williamtanuwijaya <will.tw0807@gmail.com>
2022-11-10 12:12:26 +0700

    mengerjakan soal 57

---
+++ src/Soal57.java
@@ -0,0 +1,15 @@
+public class Soal57 {
+    public static void main(String[] args) {
+        for (int i = 1 ; i <= 4 ; i++) {
+            for (int j = 1 ; j <=5 ; j++) {
+                if (i ==2 || i== 3) {
+                    System.out.print("O ");
+                } else {
+                    System.out.print("+ ");
+                }
+
+            }
+            System.out.println();
+        }
+    }
+}
