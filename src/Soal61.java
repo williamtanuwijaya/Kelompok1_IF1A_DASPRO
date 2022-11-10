da3cde8288c3d3d3d805146429f976cad07778bd
williamtanuwijaya <will.tw0807@gmail.com>
2022-11-10 12:28:53 +0700

    mengerjakan soal 61

---
+++ src/Soal61.java
@@ -0,0 +1,9 @@
+public class Soal61 {
+    public static void main(String[] args) {
+        for(int i=1;i<=4;i++) {
+            for(int j=1;j<=3;j++) {
+                System.out.print("0  ");
+            }System.out.println();
+        }
+    }
+}
