aae5fbfae8e5f17ab2506edde6613cd2a99b2bb6
williamtanuwijaya <will.tw0807@gmail.com>
2022-11-10 12:23:27 +0700

    mengerjakan soal 78

---
+++ src/Soal78.java
@@ -0,0 +1,15 @@
+public class Soal78 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 6 ;i++) {
+            for(int j=1;j <=8 ; j++) {
+                if (i == 6 || j == 8){
+                    System.out.print("0 ");
+                }else {
+                    System.out.print("* ");
+                }
+
+            }
+            System.out.println();
+        }
+    }
+}
