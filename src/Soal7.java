8fa516abcd936be0313efbe9f89cab12f2ed8a64
williamtanuwijaya <will.tw0807@gmail.com>
2022-11-10 16:42:33 +0700

    revisi soal 7

--- src/Soal7.java
+++ src/Soal7.java
@@ -1,6 +1,6 @@
 public class Soal7 {
     public static void main(String[] args) {
-        for(int i=1; i<=100;i++) {
+        for(int i=1; i<=500;i++) {
 		if(i%15==0) {
 			System.out.println(i);
 		}
