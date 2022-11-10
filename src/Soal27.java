98205369cc69617e3e197e150344f912628caa2d
williamtanuwijaya <will.tw0807@gmail.com>
2022-11-10 13:02:18 +0700

    revisi soal 27

--- src/Soal27.java
+++ src/Soal27.java
@@ -1,11 +1,9 @@
 public class Soal27 {
     public static void main(String[] args) {
-        for (int i = 2; i <= 32; i += 10) {
+        for(int i=2;i<=26;i-=5) {
             System.out.print(i + " ");
-            if (i > 2 && i <= 31) {
-                i = i - 5;
-                System.out.print(i + " ");
-            }
+            i = i + 10;
+            System.out.print(i + " ");
         }
     }
 }
