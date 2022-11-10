344c0344791124cd98f1fd2d110df76812468df5
williamtanuwijaya <will.tw0807@gmail.com>
2022-11-10 20:23:29 +0700

    revisi soal 60

--- src/Soal60.java
+++ src/Soal60.java
@@ -1,6 +1,5 @@
-import java.io.*;
 public class Soal60 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
         for (int i = 1; i <= 4; i++) {
             for (int j = 1; j <= 5; j++) {
                 if (i % 2 == 1 && j % 2 == 1) {
