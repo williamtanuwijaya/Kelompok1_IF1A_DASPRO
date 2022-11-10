b22139c02272d765ffe5580e0b6220203c0d8e2d
williamtanuwijaya <will.tw0807@gmail.com>
2022-11-10 13:01:23 +0700

    revisi soal 26

--- src/Soal26.java
+++ src/Soal26.java
@@ -1,11 +1,9 @@
 public class Soal26 {
     public static void main(String[] args) {
-        for (int i = 1; i <= 15; i += 4) {
+        for(int i=1;i<=12;i-=2) {
             System.out.print(i + " ");
-            if (i > 1 && i <= 14) {
-                i = i - 2;
-                System.out.print(i + " ");
-            }
+            i = i + 4;
+            System.out.print(i + " ");
         }
     }
 }
