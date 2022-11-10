4f4a70868e53fec55d791f98f29ba43e30c26bbb
williamtanuwijaya <will.tw0807@gmail.com>
2022-11-10 20:56:58 +0700

    revisi soal 85

--- src/Soal85.java
+++ src/Soal85.java
@@ -3,7 +3,7 @@ public class Soal85 {
         for (int i = 1; i <= 6 ;i++) {
             for(int j=1;j <= 6 ; j++) {
                 if( i == 1 || i == 4 ) {
-                    System.out.print("o ");
+                    System.out.print("0 ");
                 }else if (i == 2 || i== 5){
                     System.out.print("* ");
                 } else {
