c0e167d2517007dacc8255ebc3c9425929f0c898
Daemondollar <kingsalmon4659@gmail.com>
2022-11-11 22:04:05 +0700

    revisi soal 6

--- src/Soal6.java
+++ src/Soal6.java
@@ -1,10 +1,10 @@
 public class Soal6 {
     public static void main(String[] args) {
 		int i=1;
-        while( i<=100) {
+        while(i<=100) {
 			i++;
 		if(i%7==0) {
-			System.out.print(i + " ");
+			System.out.println(i);
 		}
 	}
   }
