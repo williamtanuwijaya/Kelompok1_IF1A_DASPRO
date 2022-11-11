bfce9e2e901e3a54bf657fa92ea4ca9cf19e7e5d
ChristoferEvan <evan200421@gmail.com>
2022-11-11 21:16:57 +0700

    revisi soal nomor 32

--- src/Soal32.java
+++ src/Soal32.java
@@ -1,11 +1,13 @@
 public class Soal32 {
     public static void main(String[] args) {
         int j = 0;
-        for(int i=1;i<=34;i+=j) {
+        int i=1;
+        while(i<=34) {
             j = j + i;
             System.out.print(i + " ");
             if (j == 34 || i == 34) break;
             System.out.print(j + " ");
+            i+=j;
         }
     }
 }
