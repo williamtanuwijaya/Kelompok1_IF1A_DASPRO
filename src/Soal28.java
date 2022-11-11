abdcb42a6a414c5f280cc299fd1b22a973d250a9
ChristoferEvan <evan200421@gmail.com>
2022-11-11 21:06:30 +0700

    revisi soal nomor 28

--- src/Soal28.java
+++ src/Soal28.java
@@ -1,9 +1,11 @@
 public class Soal28 {
     public static void main(String[] args) {
-        for(int i=5;i<=15;i+=5) {
+        int i=5;
+        while(i<=15) {
             System.out.print(i + " ");
             i = i - 3;
             System.out.print(i + " ");
+            i=i+5;
         }
     }
 }
