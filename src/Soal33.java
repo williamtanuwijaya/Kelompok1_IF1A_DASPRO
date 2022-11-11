63986e5c9ac1d44506b22c6ffddcaed0db9ec8d0
ChristoferEvan <evan200421@gmail.com>
2022-11-11 21:18:58 +0700

    revisi soal nomor 33

--- src/Soal33.java
+++ src/Soal33.java
@@ -1,11 +1,13 @@
 public class Soal33 {
     public static void main(String[] args) {
         int j = 1;
-        for(int i=0;i<=34;i+=j) {
+        int i=0;
+        while(i<=34) {
             j = j + i;
             if (i == 34)break;
             System.out.print(i + " ");
             System.out.print(j + " ");
+            i+=j;
         }
     }
 }
