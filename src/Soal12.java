de91383b4939e364c538adced49c28fd012ec15e
Daemondollar <kingsalmon4659@gmail.com>
2022-11-11 22:12:11 +0700

    revisi soal 12

--- src/Soal12.java
+++ src/Soal12.java
@@ -3,7 +3,6 @@ public class Soal12 {
         int a = 0;
         int i = 1;
         while(i <= 10) {
-
             a = a+i;
             if (i == 10) System.out.print(i + " = ");
             else System.out.print(i + " + ");
