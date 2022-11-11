ff45bff29aa9dc2f7e70aa3fbb1beaffc59aa6fa
ChristoferEvan <evan200421@gmail.com>
2022-11-11 21:10:13 +0700

    revisi soal nomor 29 dan 30

--- src/Soal30.java
+++ src/Soal30.java
@@ -1,12 +1,14 @@
 public class Soal30 {
     public static void main(String[] args) {
-        for(int i=1;i<=25;i+=3) {
+        int i=1;
+        while(i<=25) {
             System.out.print(i + " ");
             if (i == 25)break;
             i = i + 1;
             System.out.print(i + " ");
             i = i + 2;
             System.out.print(i + " ");
+            i=i+3;
         }
     }
 }
