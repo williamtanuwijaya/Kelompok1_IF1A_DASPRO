1de92b01cd46c42f6d805e2fc58327e389b13631
ChristoferEvan <evan200421@gmail.com>
2022-11-11 20:56:01 +0700

    revisi soal 11 dan 12

--- src/Soal11.java
+++ src/Soal11.java
@@ -1,6 +1,8 @@
 public class Soal11 {
     public static void main(String[] args) {
-        for(int i=1; i<=5;i++) {
+        int i=0;
+        while(i<=5) {
+            i++;
             if(i==5) {
                 System.out.print(i);
             }else if(i<5) {
--- src/Soal12.java
+++ src/Soal12.java
@@ -1,10 +1,13 @@
 public class Soal12 {
     public static void main(String[] args) {
         int a = 0;
-        for (int i = 1; i <= 10; i++) {
+        int i = 1;
+        while(i <= 10) {
+
             a = a+i;
             if (i == 10) System.out.print(i + " = ");
             else System.out.print(i + " + ");
+            i++;
            }
         System.out.print(a);
     }
