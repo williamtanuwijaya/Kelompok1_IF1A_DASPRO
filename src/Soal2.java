c5adf1873c371d4c296c98abdb723e9e74cf3214
Daemondollar <kingsalmon4659@gmail.com>
2022-11-11 21:54:28 +0700

    Revisi Soal 2

--- src/Soal2.java
+++ src/Soal2.java
@@ -2,7 +2,7 @@ public class Soal2 {
     public static void main(String[] args) {
         int i=1;
         while(i<=100){
-            System.out.print(i + " ");
+            System.out.println(i);
             ; i++;
         }
     }
