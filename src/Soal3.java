880cba4239c7d96e4107a0add6e81dcd2f98e2c0
Daemondollar <kingsalmon4659@gmail.com>
2022-11-11 21:59:07 +0700

    revisi soal 3

--- src/Soal3.java
+++ src/Soal3.java
@@ -2,7 +2,7 @@ public class Soal3 {
     public static void main(String[] args) {
         int i=100;
         while ( i>=1 ){
-            System.out.print(i + " ");
+            System.out.println(i);
             i--;
         }
     }
